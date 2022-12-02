a7cd5137a5aabf9236e392bcf834fcadafdce8ff
sudosu <jcandela@ucm.es>
2022-12-02 09:19:19 +0100

    sesion 1 02/12/22

--- Python/3-ejercicios-personalizados/1-programa-que-maneja-ips-y-cuentas-asociadas/1.2-con-expresiones-regulares/prueba-re.py
+++
@@ -1,9 +0,0 @@
-import re
-
-cadena = "192.168.1.1"
-patron = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
-match = patron.search(cadena)
-
-print("Match : {}".format(match))
-print("Retorno cadena de carecteres que coinciden RE : {}".format(match.group()))
-
--- bash-scripting/0-BASICOS/1.0-VARIABLES/variables-locales-sin-alcance.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#declaracion de variables
-opcion=0
-nombre=Marco
-
-echo "Opcion: $opcion Nombre: $nombre"
-
-exit 0
